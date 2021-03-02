aaf55d1c3ba7fa546196d1d2d70d9fc1b531b499
Andreas Lind <Andreaslind31@gmail.com>
2021-03-02 09:19:53 +0100

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Character
+    {
+        string Name { get; set; }
+        enum Race { Human = 1, Elf = 2, Dwarf = 3, Kentaur = 4, Gnome = 5 }
+        Race race { get; set; }
+        int Strength { get; set; }
+        int Dexterity { get; set; }
+        int Constitution { get; set; }
+        int Intelligence { get; set; }
+        int Wits { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Figgle;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void MenuCharacterCreation()
+        {
+            int menuNr;
+            do
+            {
+                Console.WriteLine("{ 1 } New game");
+                Console.WriteLine("{ 2 } Load game");
+                Console.WriteLine("{ 3 } Story");
+                Console.WriteLine("{ 4 } Exit");
+                Console.WriteLine();
+                bool parseNumber = int.TryParse(Console.ReadLine(), out menuNr);
+                if (parseNumber == false)
+                {
+                    Console.WriteLine("Please enter a correct number");
+                    break;
+                }
+                switch (menuNr)
+                {
+                    case 1:
+                        Console.Clear();
+                        Header();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Header();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Header();
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Header();
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Nah ah somethins gun rung ");
+                        break;
+                }
+            } while (menuNr != 4);
+        }
+        static void Header() 
+        { 
+        
+        }
+    }
+}
