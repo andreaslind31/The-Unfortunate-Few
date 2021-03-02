1dc86bc6741061f5defef9a5995cfa0a58ee353d
Andreas Lind <Andreaslind31@gmail.com>
2021-03-02 14:44:36 +0100

    Updated the Character class with logic

---
+++ ConsoleApp1/Character.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Character
+    {
+        string Name { get; set; } // föreslå namn vid skapandet beroende på race
+        private double Height // default beroende på race
+        {
+            get
+            {
+                return Height;
+            }
+            set
+            {
+                if (Race == race.Human || Race == race.Elf)
+                {
+                    Height = 1.80;
+                }
+                if (Race == race.Centaur)
+                {
+                    Height = 1.95;
+                }
+                if (Race == race.Dwarf)
+                {
+                    Height = 1.5;
+                }
+                if (Race == race.Halfling)
+                {
+                    Height = 1.1;
+                };
+            }
+        } 
+        private int Weight // default beroende på race
+        {
+            get
+            {
+                return Weight;
+            }
+            set
+            {
+                if (Race == race.Human || Race == race.Elf)
+                {
+                    Weight = 85;
+                }
+                if (Race == race.Centaur)
+                {
+                    Weight = 300;
+                }
+                if (Race == race.Dwarf)
+                {
+                    Weight = 95;
+                }
+                if (Race == race.Halfling)
+                {
+                    Weight = 50;
+                };
+            }
+        } 
+        private double Reach // default beroende på race
+        {
+            get
+            {
+                return Reach; 
+            }
+            set
+            {
+                if (Race == race.Human || Race == race.Elf)
+                {
+                    Reach = Height /2;
+                }
+                if (Race == race.Centaur)
+                {
+                    Reach = Height + 0.5;
+                }
+                if (Race == race.Dwarf || Race == race.Halfling)
+                {
+                    Reach = Height;
+                };
+            }
+        } 
+        public enum race { Human = 1, Elf = 2, Dwarf = 3, Centaur = 4, Halfling = 5 }
+        race Race { get; set; }
+
+        int Strength //+1 human / centaur, dwarf
+        {
+            get
+            { return Strength; }
+            set
+            {
+                if (Race == race.Human)
+                {Strength += 1;}
+                if (Race == race.Centaur)
+                { Strength += 1; }
+                if (Race == race.Dwarf)
+                { Strength += 1; }; 
+            } 
+        }  
+        int Dexterity //+1 human, +2 elf, halfling
+        {
+            get
+            { return Dexterity; }
+            set
+            {
+                if (Race == race.Human)
+                { Dexterity += 1; }
+                if (Race == race.Elf || Race == race.Halfling)
+                { Dexterity += 2; };
+            }
+        } 
+        int Constitution //+1 human/ dwarf, +2 centaur
+        {
+            get
+            { return Constitution; }
+            set
+            {
+                if (Race == race.Human || Race == race.Dwarf)
+                { Constitution += 1; }
+                if (Race == race.Centaur)
+                { Constitution += 2; };
+            }
+        } 
+        int Intelligence //+1 human / elf
+        {
+            get
+            { return Intelligence; }
+            set
+            {
+                if (Race == race.Human || Race == race.Elf)
+                { Intelligence += 1; };
+            }
+        } 
+        int Wisdom //+1 human / dwarf
+        {
+            get
+            { return Wisdom; }
+            set
+            {
+                if (Race == race.Human || Race == race.Dwarf)
+                { Wisdom += 1; };
+            }
+        } 
+        int Charisma //+1 human/ halfling 
+        {
+            get
+            { return Charisma; }
+            set
+            {
+                if (Race == race.Human || Race == race.Halfling)
+                { Charisma += 1; };
+            }
+        }
+
+        public EventHandler<CharacterCreation> characterCreation;
+
+        public Character(string name, race race, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
+        {
+            this.Name = name;
+            this.Race = race;
+            this.Strength = strength;
+            this.Dexterity = dexterity;
+            this.Constitution = constitution;
+            this.Intelligence = intelligence;
+            this.Wisdom = wisdom;
+            this.Charisma = charisma;
+        }
+    }
+    public class CharacterCreation : EventArgs { }
+}
---
+++ ConsoleApp1/DifferentMenus.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.IO;
+using Figgle;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class DifferentMenus
+    {
+    /// 
+    /// Start up menus
+    /// 
+        public static async Task MainMenu()
+        {
+            Console.Clear();
+            Header();
+            int menuNr;
+            do
+            {
+                Console.WriteLine("{ 1 } New game");
+                Console.WriteLine("{ 2 } Load game");
+                Console.WriteLine("{ 3 } Story");
+                Console.WriteLine("{ 4 } Exit");
+                Console.WriteLine();
+
+                bool parseNumber = int.TryParse(Console.ReadLine(), out menuNr);
+                if (parseNumber == false)
+                {
+                    Console.WriteLine("Please enter a correct number");
+                    break;
+                }
+                switch (menuNr)
+                {
+                    case 1:
+                        Console.Clear();
+                        Header();
+                        await CharacterCreation();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Header();
+
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Header();
+                        await StoryMenu();
+                        break;
+                    case 4:
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Nope");
+                        break;
+                }
+            } while (menuNr != 4);
+        }
+        public static async Task StoryMenu()
+        {
+            Console.Clear();
+            Header();
+            int menuNr;
+            do
+            {
+
+                Console.WriteLine("{ 1 } The story so far\n");
+                Console.WriteLine("Playable races: ");
+                Console.WriteLine("{ 2 } Centaurs");
+                Console.WriteLine("{ 3 } Dwarfs");
+                Console.WriteLine("{ 4 } Elfs");
+                Console.WriteLine("{ 5 } Halflings");
+                Console.WriteLine("{ 6 } Humans");
+                bool parseNumber = int.TryParse(Console.ReadLine(), out menuNr);
+                if (parseNumber == false)
+                {
+                    await MainMenu();
+                    break;
+                }
+                switch (menuNr)
+                {
+                    case 1:
+                        Console.Clear();
+                        Header();
+                        await StorySoFar();
+                        await BackToStoryMenu();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Header();
+                        await AboutCentaurs();
+                        await BackToStoryMenu();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Header();
+                        await AboutDwarfs();
+                        await BackToStoryMenu();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Header();
+                        await AboutElfs();
+                        await BackToStoryMenu();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Header();
+                        await AboutHalflings();
+                        await BackToStoryMenu();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Header();
+                        await AboutHumans();
+                        await BackToStoryMenu();
+                        break;
+                    default:
+
+                        break;
+                }
+            } while (menuNr != 7);
+        }
+        public static async Task StorySoFar()
+        {
+            String filename = "StorySoFar.txt";
+            Char[] buffer;
+
+            using (var sr = new StreamReader(filename))
+            {
+                buffer = new Char[(int)sr.BaseStream.Length];
+                await sr.ReadAsync(buffer, 0, (int)sr.BaseStream.Length);
+            }
+
+            Console.WriteLine(new String(buffer));
+        }
+        public static async Task AboutCentaurs()
+        {
+            String filename = "CentaursStory.txt";
+            Char[] buffer;
+
+            using (var sr = new StreamReader(filename))
+            {
+                buffer = new Char[(int)sr.BaseStream.Length];
+                await sr.ReadAsync(buffer, 0, (int)sr.BaseStream.Length);
+            }
+
+            Console.WriteLine(new String(buffer));
+        }
+        public static async Task AboutDwarfs()
+        {
+            String filename = "DwarfsStory.txt";
+            Char[] buffer;
+
+            using (var sr = new StreamReader(filename))
+            {
+                buffer = new Char[(int)sr.BaseStream.Length];
+                await sr.ReadAsync(buffer, 0, (int)sr.BaseStream.Length);
+            }
+
+            Console.WriteLine(new String(buffer));
+        }
+        public static async Task AboutElfs()
+        {
+            String filename = "ElfsStory.txt";
+            Char[] buffer;
+
+            using (var sr = new StreamReader(filename))
+            {
+                buffer = new Char[(int)sr.BaseStream.Length];
+                await sr.ReadAsync(buffer, 0, (int)sr.BaseStream.Length);
+            }
+
+            Console.WriteLine(new String(buffer));
+        }
+        public static async Task AboutHalflings()
+        {
+            String filename = "HalflingsStory.txt";
+            Char[] buffer;
+
+            using (var sr = new StreamReader(filename))
+            {
+                buffer = new Char[(int)sr.BaseStream.Length];
+                await sr.ReadAsync(buffer, 0, (int)sr.BaseStream.Length);
+            }
+
+            Console.WriteLine(new String(buffer));
+        }
+        public static async Task AboutHumans()
+        {
+            String filename = "HumansStory.txt";
+            Char[] buffer;
+
+            using (var sr = new StreamReader(filename))
+            {
+                buffer = new Char[(int)sr.BaseStream.Length];
+                await sr.ReadAsync(buffer, 0, (int)sr.BaseStream.Length);
+            }
+
+            Console.WriteLine(new String(buffer));
+
+        }
+        public static async Task BackToStoryMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\nEnter or escape to return\n");
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+            if (consoleKeyInfo.Key == ConsoleKey.Enter || consoleKeyInfo.Key == ConsoleKey.Escape)
+            {
+                await StoryMenu();
+            }
+        }
+        public static async Task CharacterCreation()
+        {
+            Console.Clear();
+            Header();
+            int menuNr;
+            do
+            {
+                Console.WriteLine("{ 1 } New character");
+                Console.WriteLine("{ 2 } Check out the origin characters");
+                Console.WriteLine("{ 3 } Back");
+                Console.WriteLine();
+
+                bool parseNumber = int.TryParse(Console.ReadLine(), out menuNr);
+                if (parseNumber == false)
+                {
+                    Console.WriteLine("Enter a correct number");
+                    break;
+                }
+                switch (menuNr)
+                {
+                    case 1:
+                        Console.Clear();
+                        Header();
+
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Header();
+
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Header();
+                        await StoryMenu();
+                        break;
+                    default:
+                        Console.WriteLine("Nope");
+                        break;
+                }
+            } while (menuNr != 4);
+        }
+        static void Header()
+        {
+            Console.WriteLine(FiggleFonts.Ogre.Render("The Unfortunate Few!"));
+            Console.WriteLine("\n ");
+        }
+    }
+}
